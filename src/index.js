cfc7804aa64225b3a420b50773318809a2bffea7
klenilsonrox <klenilsoncoc@gmail.com>
2024-10-29 19:30:36 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import { createServer } from 'http'; // Importar para criar o servidor HTTP
-import { Server } from 'socket.io';  // Importar o Socket.io
 import RouterCategory from './routes/category-routes.js';
 import RouterUsers from './routes/user-routes.js';
 import RouterProducts from './routes/product-route.js';
@@ -10,7 +8,7 @@ import RouterOrders from './routes/order-routes.js';
 
 const port = process.env.PORT || 4001;
 const app = express();
-const server = createServer(app); // Cria o servidor HTTP
+
 
 
 app.use(express.json());
